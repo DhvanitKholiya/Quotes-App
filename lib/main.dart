4477ea29e927ee716493aab64d6a6c7bb6b8d8f9
DhvanitKholiya <dhvanitkholiya28@gmail.com>
2023-05-03 12:46:09 +0530

    work on database

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:quotesapp/views/screens/home_page.dart';
 import 'package:quotesapp/views/screens/quotes_page.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+      MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
       useMaterial3: true,
--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   void changeQuote() {
     setState(() {
-      indexVal = (indexVal + 1) % Global.allQuotes.length;
+      indexVal = (indexVal + 1) + Global.allQuotes.length;
       Global.currentQuote = Global.allQuotes[indexVal];
     });
   }
@@ -25,9 +25,9 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    Global.currentQuote = Global.allQuotes[indexVal];
-    timer = Timer.periodic(Duration(seconds: 1), (Timer) => changeQuote());
-    print(indexVal);
+    // Global.currentQuote = Global.allQuotes[indexVal];
+    timer = Timer.periodic(const Duration(seconds: 1), (Timer t) => changeQuote());
+    print(timer);
   }
 
   @override
@@ -38,95 +38,95 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         appBar: AppBar(
           title: const Text("Quotes On Your Mood"),
           centerTitle: true,
-          // bottom: TabBar(padding:const EdgeInsets.all(8), tabs: [
-          //   const Text("Positive Quotes"),
-          //   const Text("Inspire Quotes"),
-          //   const Text("Life Quotes"),
-          //   const Text("Sad Quotes"),
-          // ]),
+          bottom: const TabBar(padding: EdgeInsets.all(8), tabs: [
+             Text("Positive Quotes"),
+             Text("Inspire Quotes"),
+             Text("Life Quotes"),
+             Text("Sad Quotes"),
+          ]),
         ),
-        body: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          Global.allQuotes = Global.positiveQuotes;
-                        });
-                      },
-                      child: Text("Positive Quotes")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          Global.allQuotes = Global.inspirationalQuotes;
-                        });
-                      },
-                      child: Text("Inspire Quotes")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          Global.allQuotes = Global.lifeQuotes;
-                        });
-                      },
-                      child: Text("Life Quotes")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          Global.allQuotes = Global.sadQuotes;
-                        });
-                      },
-                      child: Text("Sad Quotes")),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Text(Global.currentQuote),
-                ],
-              ),
-            ),
-          ],
-        ),
-        // body: Container(
-        //   padding: const EdgeInsets.all(10),
-        //   decoration: const BoxDecoration(
-        //     image: DecorationImage(
-        //         image: NetworkImage(
-        //             'https://i.pinimg.com/originals/78/4f/cc/784fcc107b893db18d0ad22db04f6f15.gif'),
-        //         fit: BoxFit.cover,
-        //         opacity: 0.8),
-        //   ),
-        //   child: TabBarView(
-        //     children: [
-        //       Center(
-        //           child: Text(
-        //         Global.positiveQuotes[indexVal],
-        //         style: const TextStyle(color: Colors.black, fontSize: 24),
-        //       )),
-        //       Center(
-        //           child: Text(
-        //         Global.inspirationalQuotes[indexVal],
-        //         style: const TextStyle(color: Colors.black, fontSize: 24),
-        //       )),
-        //       Center(
-        //           child: Text(
-        //         Global.lifeQuotes[indexVal],
-        //         style: const TextStyle(color: Colors.black, fontSize: 24),
-        //       )),
-        //       Center(
-        //           child: Text(
-        //         Global.sadQuotes[indexVal],
-        //         style: const TextStyle(color: Colors.black, fontSize: 24),
-        //       )),
-        //     ],
-        //   ),
+        // body: Column(
+        //   children: [
+        //     SingleChildScrollView(
+        //       scrollDirection: Axis.horizontal,
+        //       child: Row(
+        //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //         children: [
+        //           ElevatedButton(
+        //               onPressed: () {
+        //                 setState(() {
+        //                   Global.allQuotes = Global.positiveQuotes;
+        //                 });
+        //               },
+        //               child: Text("Positive Quotes")),
+        //           ElevatedButton(
+        //               onPressed: () {
+        //                 setState(() {
+        //                   Global.allQuotes = Global.inspirationalQuotes;
+        //                 });
+        //               },
+        //               child: Text("Inspire Quotes")),
+        //           ElevatedButton(
+        //               onPressed: () {
+        //                 setState(() {
+        //                   Global.allQuotes = Global.lifeQuotes;
+        //                 });
+        //               },
+        //               child: Text("Life Quotes")),
+        //           ElevatedButton(
+        //               onPressed: () {
+        //                 setState(() {
+        //                   Global.allQuotes = Global.sadQuotes;
+        //                 });
+        //               },
+        //               child: Text("Sad Quotes")),
+        //         ],
+        //       ),
+        //     ),
+        //     Container(
+        //       padding: EdgeInsets.all(10),
+        //       child: Column(
+        //         children: [
+        //           Text(Global.lifeQuotes[0]),
+        //         ],
+        //       ),
+        //     ),
+        //   ],
         // ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://i.pinimg.com/originals/78/4f/cc/784fcc107b893db18d0ad22db04f6f15.gif'),
+                fit: BoxFit.cover,
+                opacity: 0.8),
+          ),
+          child: TabBarView(
+            children: [
+              Center(
+                  child: Text(
+                Global.positiveQuotes[indexVal],
+                style: const TextStyle(color: Colors.black, fontSize: 24),
+              )),
+              Center(
+                  child: Text(
+                Global.inspirationalQuotes[indexVal],
+                style: const TextStyle(color: Colors.black, fontSize: 24),
+              )),
+              Center(
+                  child: Text(
+                Global.lifeQuotes[indexVal],
+                style: const TextStyle(color: Colors.black, fontSize: 24),
+              )),
+              Center(
+                  child: Text(
+                Global.sadQuotes[indexVal],
+                style: const TextStyle(color: Colors.black, fontSize: 24),
+              )),
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             setState(() {
